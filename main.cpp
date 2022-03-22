1e61f78c9f3777514991b4a5acb3d0be2d4deed9
qwertygaerty <54385591+qwertygaerty@users.noreply.github.com>
2022-03-22 18:55:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 /******************************************************************************
-
                              Крутой справочник
-
 *******************************************************************************/
 
 #include <iostream>
@@ -43,65 +41,71 @@ int getFirstNonSpaceSymbol(string mass[4]) {
     return count;
 }
 
-string getStringDelimiter(string s) {
-    string mass[4]= {"n", "n", "n", "n"};
+string getStringDelimiter(string s, string mass[4]) {
+    
     std::istringstream instr(s);
     string w;
     int count;
     while (instr >> w) {
         count = getFirstNonSpaceSymbol(mass);
-
-
         mass[count] = w;
     }
-
-     cout << "ID: " << mass[0] << "\tName: " << mass[1] << "\tSurName: " << mass[2] << "\t\tPhone: " << mass[3] << endl;
-
-   return "";
+    
+   return *mass;
 }
 
-string getClassItem(string t) {
-    string word;
-    int id;
-    string name = "";
-    string surname;
-    string phone;
-    int flag = 0;
-    getStringDelimiter(t);
-    // cout << "ID: " << id << "\tName: " << name << "\tSurName: " << surname << "\t\tPhone: " << phone << endl;
+Tele getClassItem(string t) {
+    string mass[4]= {"n", "n", "n", "n"};
     
-    return "";
+    
+    getStringDelimiter(t, mass);
+    
+    Tele item;
+    item.id =  stoi(mass[0]);
+    item.name = mass[1];
+    item.surname = mass[2];
+    item.phone = mass[3];
+    
+    return item;
 }
 
 void mounted() {
     int count = 0;
+    int n = 0;
     string teleString = "";
     char c;
-    string teleMass[1000];
+   
+    
     while (!feof(f))
         if ((c = fgetc(f)) == ';') {
             count++;
-            
-            teleMass[count] = getClassItem(teleString);
+        } 
+        
+    isFile();
+    Tele teleMass[count];
+    
+    while (!feof(f))
+        if ((c = fgetc(f)) == ';') {
+            teleMass[n] = getClassItem(teleString);
+            n++;
             teleString = "";
         } else {
             teleString += c;
         }
+        
+    
+        
+for(int i = 0; i< count;i++) {
+    cout << "ID: " << teleMass[i].id << "\tName: " << teleMass[i].name << "\tSurName: " << teleMass[i].surname << "\t\tPhone: " << teleMass[i].phone << endl;
+}
+
+
     
 }
 
 int main() {
-    
     isFile();
     mounted();
-
-    //   Tele dan;
-    //   dan.id = 1;
-    //   dan.name = "Dan";
-    //   dan.surname = "Ahagna";
-
-    //   dan.phone = "8348345676";
-    //   dan.print();
 
     return 0;
 }
