b63e6e19f50fcde7d0391d236e22b58888712bc4
y <qwerty@gmail.com>
2022-03-22 11:18:27 +0700

    third commit

--- main.cpp
+++ main.cpp
@@ -5,14 +5,12 @@
 *******************************************************************************/
 
 #include <iostream>
-
 #include <string>
-
 #include <fstream>
-
 using std::string;
 using std::cout;
 using std::endl;
+FILE *f;
 
 class Tele {
   public:
@@ -26,32 +24,43 @@ class Tele {
 };
 
 FILE isFile() {
-  FILE *f;
   if ((f = fopen("text.txt", "r")) == NULL) {
     std::ofstream oFile("text.txt");
-
-    return *f;
   };
   return *f;
 }
 
 void mounted() {
+    int count = 0;
+    string tele = "";
+    char c;
+    while (!feof(f)) if ((c=fgetc(f))=='\n') {
+        count++;
+        cout << tele << endl;
+        tele = "";
+    } else {
+        tele+=c;
+    }
+
+
 
 }
 
 int main() {
 
-  FILE file = isFile();
+    isFile();
 
-  string teleFile = "text.txt";
+  mounted();
 
-  Tele dan;
-  dan.id = 1;
-  dan.name = "Dan";
-  dan.surname = "Ahagna";
 
-  dan.phone = "8348345676";
-  dan.print();
+
+//   Tele dan;
+//   dan.id = 1;
+//   dan.name = "Dan";
+//   dan.surname = "Ahagna";
+
+//   dan.phone = "8348345676";
+//   dan.print();
 
   return 0;
 }
