61d40bf98f5ee53eeae54782c751290c8bd8c786
qwertygaerty <54385591+qwertygaerty@users.noreply.github.com>
2022-04-04 17:42:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,9 +14,10 @@ using namespace std;
 using std::string;
 using std::cout;
 using std::endl;
-void printStart();
 
-FILE * f;
+void printStart(int count);
+
+FILE *f;
 
 string toString(int n) {
     std::ostringstream ss;
@@ -31,29 +32,35 @@ string toStringLong(long n) {
 }
 
 class Tele {
-    public:
-        string name;
+public:
+    string name;
     string surname;
     int id;
     long phone;
+
     void print() {
 
-        cout << "---------------------------------------------------------------------------------------------------------" << endl;
+        cout
+                << "---------------------------------------------------------------------------------------------------------"
+                << endl;
 
-        cout << "ID: " << id << "\tName: " << name << "\tSurName: " << surname << "\t\t|" << "\t\tPhone: " << phone << endl;
+        cout << "ID: " << id << "\tName: " << name << "\tSurName: " << surname << "\t\t|" << "\t\tPhone: " << phone
+             << endl;
 
-        cout << "---------------------------------------------------------------------------------------------------------" << endl;
+        cout
+                << "---------------------------------------------------------------------------------------------------------"
+                << endl;
     }
 
     string toLine() {
-        return toString(this -> id) + ". " + this -> name + " " + this -> surname + " " + toStringLong(this -> phone) + ";";
+        return toString(this->id) + ". " + this->name + " " + this->surname + " " + toStringLong(this->phone) + ";";
     }
 
     void restore(int id, string name, string surname, long phone) {
-        if (id != -1) this -> id = id;
-        if (name != " ") this -> name = name;
-        if (surname != " ") this -> surname = surname;
-        if (phone != -1) this -> phone = phone;
+        if (id != -1) this->id = id;
+        if (name != " ") this->name = name;
+        if (surname != " ") this->surname = surname;
+        if (phone != -1) this->phone = phone;
     }
 };
 
@@ -74,6 +81,11 @@ int getFirstNonSpaceSymbol(string mass[4]) {
     return count;
 }
 
+bool isNumberOfPhone(long number) {
+    if (number > 999999999 && number < 10000000000) return true;
+    return false;
+}
+
 string getStringDelimiter(string s, string mass[4]) {
     std::istringstream instr(s);
     string w;
@@ -88,10 +100,10 @@ string getStringDelimiter(string s, string mass[4]) {
 
 Tele getClassItem(string t) {
     string mass[4] = {
-        "n",
-        "n",
-        "n",
-        "n"
+            "n",
+            "n",
+            "n",
+            "n"
     };
 
     getStringDelimiter(t, mass);
@@ -115,7 +127,7 @@ int getLengthFile(int count) {
     return count;
 }
 
-Tele getClassMass(Tele * teleMass) {
+Tele getClassMass(Tele *teleMass) {
     string teleString = "";
     int n = 0;
     char c;
@@ -135,7 +147,7 @@ void mounted() {
     int count = 0;
     count = getLengthFile(count);
 
-    if (count == 0) printStart();
+    if (count == 0) printStart(0);
 
     Tele teleMass[count];
     getClassMass(teleMass);
@@ -144,7 +156,7 @@ void mounted() {
         teleMass[i].print();
     }
 
-    printStart();
+    printStart(count);
 
 }
 
@@ -171,12 +183,12 @@ void searchInFile(string word_source, string word_dest) {
                 count++;
 
                 temp += str.substr(position_start,
-                    position - position_start);
+                                   position - position_start);
                 temp += word_dest;
                 position += word_source.size();
             } else {
                 temp += str.substr(position_start,
-                    position - position_start);
+                                   position - position_start);
             }
         }
         temp += "\n";
@@ -189,7 +201,46 @@ void searchInFile(string word_source, string word_dest) {
     fout.close();
 }
 
-void addToFile() {
+
+string isOnlyString(std::string query) {
+    string value;
+    std::cout << query.c_str();
+    while (!(std::cin >> value)) {
+        if (std::cin.eof()) { throw "eof"; }
+        std::cin.clear(); //Сбрасываем флаг ошибки, если таковая была
+        std::cin.ignore(1000, '\n'); //Игнорируем оставшиеся в потоке данные
+        std::cout << "Try again: ";
+    }
+    return value;
+}
+
+long isOnlyLong(std::string query) {
+    long value;
+    std::cout << query.c_str();
+    while (!(std::cin >> value) || !isNumberOfPhone(value)) {
+        if (std::cin.eof()) { throw "eof"; }
+        std::cin.clear(); //Сбрасываем флаг ошибки, если таковая была
+        std::cin.ignore(1000, '\n'); //Игнорируем оставшиеся в потоке данные
+        std::cout << "Введите номер телефона 10 цифр: ";
+    }
+    return value;
+}
+
+
+int isOnlyInt(std::string query) {
+    int value;
+    std::cout << query.c_str();
+    while (!(std::cin >> value)) {
+        if (std::cin.eof()) { throw "eof"; }
+        std::cin.clear(); //Сбрасываем флаг ошибки, если таковая была
+        std::cin.ignore(1000, '\n'); //Игнорируем оставшиеся в потоке данные
+        std::cout << "Введите корректный id: ";
+    }
+    return value;
+}
+
+
+void addToFile(int numberOfPhones) {
     string fileText;
     Tele temp;
     isFile();
@@ -198,49 +249,40 @@ void addToFile() {
 
     for (int i = 0; i < 4; i++) {
         switch (i) {
-        case 0: {
-            cout << "Введите " << "id" << endl;
-            while (true) {
-                if ((std::cin >> temp.id).good()) break;
-                if (std::cin.fail()) {
-                    std::cin.clear();
-                    cout << "Введите число" << endl;
-                    std::cin.ignore();
-                }
+            case 0: {
+                temp.id = numberOfPhones + 1;
+                cout << temp.id << " - id" << endl;
+                break;
             }
-            break;
-        }
-        case 1: {
-            cout << "Введите " << "name" << endl;
-            std::cin >> temp.name;
-            break;
-        }
-        case 2: {
-            cout << "Введите " << "surname" << endl;
-            std::cin >> temp.surname;
-            break;
-        }
+            case 1: {
+                temp.name = isOnlyString("Введите name: ");
+                break;
+            }
+            case 2: {
+                temp.surname = isOnlyString("Введите surname: ");
+                break;
+            }
 
-        case 3: {
-            cout << "Введите " << "phone" << endl;
-            std::cin >> temp.phone;
-            break;
-        }
+            case 3: {
+                temp.phone = isOnlyLong("Введите phone: ");
+                break;
+            }
         }
     }
 
     fileText = temp.toLine();
-
     vmdelet_out << fileText << '\n';
     vmdelet_out.close();
     mounted();
 }
 
 void removeToFile() {
+    bool isId = true;
     int id;
     int count = 0;
-    cout << "Введите id для Удаления" << endl;
-    std::cin >> id;
+
+    id = isOnlyInt("Введите id для Удаления: ");
+
     count = getLengthFile(count);
     Tele teleMass[count];
     getClassMass(teleMass);
@@ -251,14 +293,19 @@ void removeToFile() {
             cout << "Удалено" << endl;
             cout << str << endl;
             searchInFile(str, " ");
+            isId = true;
+        } else {
+            isId = false;
         }
     }
+
+    if (!isId) removeToFile();
 
     mounted();
 
 }
 
-void printStart() {
+void printStart(int count) {
 
     int choose;
     cout << "Введите 0 для добавления" << endl;
@@ -268,15 +315,15 @@ void printStart() {
     std::cin >> choose;
 
     switch (choose) {
-    case 0: {
-        addToFile();
-    }
-    case 1: {
-        removeToFile();
-    }
-    case 2: {
+        case 0: {
+            addToFile(count);
+        }
+        case 1: {
+            removeToFile();
+        }
+        case 2: {
 
-    }
+        }
 
     }
 
